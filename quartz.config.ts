0eb4ebd3415d3c7f807b95831bbb3a15313ff6ec
DElmore2004 <delmore2004@gmail.com>
2026-02-13 09:16:17 -0500

    Quartz sync: Feb 13, 2026, 9:16 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "delmore2004.github.io/Kalzar/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
